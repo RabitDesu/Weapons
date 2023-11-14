d170a4bb20c9fef863b06ebaeaf88f890397a9a2
RabitDesu <119028480+RabitDesu@users.noreply.github.com>
2023-11-15 04:05:20 +0900

    Add files via upload

---
+++ src/client/java/com/weapons/WeaponsClient.java
@@ -0,0 +1,10 @@
+package com.weapons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeaponsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
